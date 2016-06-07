8be125e79ca8082a98cbf518fa26cb6f6e0e11a4
mhptsui <mhptsui@gmail.com>
2016-06-07 13:59:23 +0800

    Add files via upload

---
+++ Test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test::Application.config.secret_token = '8ceeca1309cb5cda154f83aa10a68d0e3c7dc55595c15c3b663ce343c6c5b53ecb94ea1835c8119e47423cce0136571a4a67d7d91466df5e330db6daec1a2d3d'
